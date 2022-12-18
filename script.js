297963d1b836e4050bfc43b231ff53150439b997
Mikhal <l1bredim@ukr.net>
2022-12-18 18:07:00 +0200

    first

---
+++ script.js
@@ -0,0 +1,63 @@
+const dom = {
+    new: document.getElementById('new'),
+    add: document.getElementById('add'),
+    tasks: document.getElementById('tasks')
+}
+
+const tasks = []
+
+dom.add.onclick = () => {
+    const newTaskText = dom.new.value
+    if(newTaskText && isNotHaveTask(newTaskText, tasks)) {
+        addTask(newTaskText, tasks)
+        dom.new.value = ''
+        tasksRender(tasks)
+    }
+}
+
+function addTask(text, list) {
+    let currentTime = Date.now()
+    const task = {
+        id: currentTime,
+        text,
+        isComplete: false
+    }
+    list.push(task)
+
+}
+
+function isNotHaveTask(text, list) {
+    let isNotHaveTask = true
+
+    list.forEach((task) => {
+        if (task.text === text) {
+            alert('Task already use')
+            isNotHaveTask = false
+        }
+    })
+    return isNotHaveTask
+}
+
+function tasksRender(list) {
+    let htmlList = ''
+
+    list.forEach(task => {
+        const cls = task.isComplete 
+        ? 'todo__task todo__task_complete'
+        : 'todo__task'
+        const checked = task.isComplete ? 'checked' : ''
+        const taskHtml = `
+        <div id="${task.id}" class="${cls}">
+                <label class="todo__checkbox" >
+                    <input type="checkbox" ${checked}">
+                    <div></div>
+                </label>
+                    <div class="todo__task-title">${task.text}</div>
+                <div class="todo__task-del">Delete</div>
+            </div>
+        `
+        htmlList = htmlList + taskHtml
+    })
+
+    dom.tasks.innerHTML = htmlList
+}
